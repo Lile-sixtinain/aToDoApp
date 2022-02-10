b54d3f3e25aef2835b0bf0a13fa2a18f61df4cea
Lilen <zaratelilen@gmail.com>
2022-02-10 18:02:42 -0300

    html y app

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ document.querySelector('#push').onclick = function(){
       alert("Please Enter a Task")
   }
   else{
-      document.querySelector('#tasks').innerHTML += `
-          <div class="task">
-              <p id="taskname">
-                  ${document.querySelector('#newtask input').value}
-              </p>
-              <button type="button" class="btn-close" aria-label="Close"></button>
-          </div>
-      `;
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <p id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </p>
+                <button type="button" class="btn-close" aria-label="Close"></button>
+            </div>
+        `;
+            //BUTTON DELETE
+            var delete_task = document.querySelectorAll(".btn-close");
+            for(var i=0; i< delete_task.length; i++){
+                delete_task[i].onclick = function(){
+                this.parentNode.remove();
+                }
+            }
+      } 
 
-  }
 }
